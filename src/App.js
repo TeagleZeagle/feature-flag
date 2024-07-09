de8d23b94fb0d68d597d0a01bfa320699d5e44d1
Kaan Kani İnce <kkince@newmind.ai>
2024-07-09 16:24:21 +0300

    Feature-flag created.

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import FeatureFlagGlobalState from "./components/feature-flag/context";
+import FeatureFlags from "./components/feature-flag";
+
+function App() {
+  return (
+    <div className="App">
+      {/* Feature Flag Implementation */}
+      <FeatureFlagGlobalState>
+        <FeatureFlags />
+      </FeatureFlagGlobalState>
+    </div>
+  );
+}
+
+export default App;
